4e9dbdf6dfc5afd4df90dbf92db91dcac652a1a7
edualt <eduardosebastianhdz503@gmail.com>
2022-10-13 20:30:04 -0500

    feat(api): add perform_create method

--- apps/advocates/api/viewsets.py
+++ apps/advocates/api/viewsets.py
@@ -1,7 +1,15 @@
-from rest_framework import viewsets
-from .serializers import AdvocateSerializer
 from apps.advocates.models import Advocate
+from apps.companies.models import Company
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from .serializers import AdvocateSerializer
+
 
 class AdvocateViewSet(viewsets.ModelViewSet):
     queryset = Advocate.objects.all()
     serializer_class = AdvocateSerializer
+    
+    def perform_create(self, serializer):
+        company = Company.objects.get(pk=self.request.data['company'])
+        serializer.save(company=company)
