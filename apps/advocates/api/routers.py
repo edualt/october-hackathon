4b3c71e8404bcc708d926e9421d923a950672f18
edualt <eduardosebastianhdz503@gmail.com>
2022-10-13 20:31:07 -0500

    feat(api): add routers

--- apps/advocates/api/routers.py
+++ apps/advocates/api/routers.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
+
 from .viewsets import AdvocateViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r'advocates', AdvocateViewSet)
+router.register(r'', AdvocateViewSet, basename='advocates')
 
 urlpatterns = router.urls
