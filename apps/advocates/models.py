dd891c2196dede944e0ad522fd47084d561288bc
edualt <eduardosebastianhdz503@gmail.com>
2022-10-13 20:32:24 -0500

    feat(model): add foreign key field

--- apps/advocates/models.py
+++ apps/advocates/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+from apps.companies.models import Company
+
+
 class Advocate(models.Model):
     name = models.CharField(max_length=100)
     # profile_pic = models.ImageField(upload_to='advocates/')
     short_bio = models.CharField(max_length=100)
     long_bio = models.TextField()
     years_of_experience = models.IntegerField()
-    # company = models.ForeignKey(Company)
+    company = models.ForeignKey('companies.Company', related_name='advocates', on_delete=models.CASCADE, null=True)
+    links = models.JSONField(default=dict, blank=True, null=True)
 
     def __str__(self):
         return self.name
