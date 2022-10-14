58b03c6a9918accf3c82de76b12394a40268da99
edualt <eduardosebastianhdz503@gmail.com>
2022-10-13 20:27:30 -0500

    feat(company): add viewset to write custom fields

---
+++ apps/companies/api/viewsets.py
@@ -0,0 +1,17 @@
+
+from apps.companies.api.serializers import CompanySerializer
+from apps.companies.models import Company
+from rest_framework.viewsets import ModelViewSet
+
+
+class CompanyViewSet(ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+            company = serializer.instance
+            company.href = f'/companies/{company.id}/'
+            company.save()
+            return company
