3b427da110398c5a502f00c36fc56ac72f7bb6f1
edualt <eduardosebastianhdz503@gmail.com>
2022-10-13 20:30:54 -0500

    feat(api): change field settings

--- apps/advocates/api/serializers.py
+++ apps/advocates/api/serializers.py
@@ -1,7 +1,10 @@
-from rest_framework import serializers
 from apps.advocates.models import Advocate
+from rest_framework import serializers
+
 
 class AdvocateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Advocate
-        fields = '__all__'
+        fields = ['id', 'name', 'short_bio', 'long_bio', 'years_of_experience', 'company', 'links']
+        depth = 1
