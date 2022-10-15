1e0f18d8d272e7b3913478322c3a489161925159
edualt <eduardosebastianhdz503@gmail.com>
2022-10-14 23:11:59 -0500

    feat(api): re-order serializer fields

--- apps/advocates/api/serializers.py
+++ apps/advocates/api/serializers.py
@@ -1,10 +1,12 @@
 from apps.advocates.models import Advocate
+from apps.companies.models import Company
 from rest_framework import serializers
 
 
 class AdvocateSerializer(serializers.ModelSerializer):
+    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())
 
     class Meta:
         model = Advocate
-        fields = ['id', 'name', 'short_bio', 'long_bio', 'years_of_experience', 'company', 'links']
+        fields = ['id', 'name', 'short_bio', 'long_bio', 'years_of_experience', 'links', 'company']
         depth = 1
