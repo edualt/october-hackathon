054412f662985bb03345c0273e46f8e24dfe8409
edualt <eduardosebastianhdz503@gmail.com>
2022-10-14 23:12:12 -0500

    feat(api): remove perform_create method

--- apps/advocates/api/viewsets.py
+++ apps/advocates/api/viewsets.py
@@ -1,5 +1,6 @@
 from apps.advocates.models import Advocate
 from apps.companies.models import Company
+from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -9,7 +10,6 @@ from .serializers import AdvocateSerializer
 class AdvocateViewSet(viewsets.ModelViewSet):
     queryset = Advocate.objects.all()
     serializer_class = AdvocateSerializer
-    
-    def perform_create(self, serializer):
-        company = Company.objects.get(pk=self.request.data['company'])
-        serializer.save(company=company)
+
+
+
