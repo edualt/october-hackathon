e75fd109bbff75715c96cc31f59c4e6499ea1457
edualt <eduardosebastianhdz503@gmail.com>
2022-10-20 16:00:18 -0500

    feat(serializers): add advocates relationship

--- apps/companies/api/serializers.py
+++ apps/companies/api/serializers.py
@@ -1,16 +1,15 @@
-from apps.advocates.api.serializers import AdvocateSerializer
 from apps.advocates.models import Advocate
 from apps.companies.models import Company
 from rest_framework import serializers
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    advocates = serializers.PrimaryKeyRelatedField(queryset=Advocate.objects.all(), many=True)
         
     class Meta:
         model = Company
-        fields = ['id', 'name', 'href', 'summary', 'advocates']
+        fields = ['id', 'name', 'logo','href', 'summary', 'advocates']
         extra_kwargs = {
             'href': {'read_only': True}
         }
         depth = 1
-    
