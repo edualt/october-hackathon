79f5e1665a27dbe679346ce0a9711957e58ff661
edualt <eduardosebastianhdz503@gmail.com>
2022-10-20 16:14:04 -0500

    feat(models): add logo field and advocates relationship and ordering

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+from apps.advocates.models import Advocate
+
+
+def upload_to(instance, filename):
+    return 'companies/{filename}'.format(filename=filename)
 
 class Company(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-    # logo = models.ImageField(upload_to='companies/')
+    logo = models.ImageField(upload_to=upload_to, blank=True, null=True)
     href = models.URLField(max_length=200, null=False, blank=False)
     summary = models.TextField()
+    advocates = models.ManyToManyField(Advocate, related_name='companies')
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self):
         return self.name
