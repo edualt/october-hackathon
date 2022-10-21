7c10ca9675e92936e51ba4d0e72d80e38664a375
edualt <eduardosebastianhdz503@gmail.com>
2022-10-20 19:12:04 -0500

    feat(settings/production): production ready

--- core/settings/production.py
+++ core/settings/production.py
@@ -17,6 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
+        'URL': env.str('DB_URL'),
         'PASSWORD': env.str('DB_PASSWORD'),
         'HOST': env.str('DB_HOST'),
         'PORT': env.str('DB_PORT'),
