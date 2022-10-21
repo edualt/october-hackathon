0ed0bc26a1c866550c88efbaa9fa1dd012a88a73
edualt <eduardosebastianhdz503@gmail.com>
2022-10-20 19:05:59 -0500

    feat(entrypoint): add enviroment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Check if database is up
-python manage.py wait_for_db --settings=core.settings.local
+python manage.py wait_for_db --settings=core.settings.${enviroment}
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigrations --settings=core.settings.local
-python manage.py migrate --settings=core.settings.local
+python manage.py makemigrations --settings=core.settings.${enviroment}
+python manage.py migrate --settings=core.settings.${enviroment}
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000 --settings=core.settings.local
+python manage.py runserver 0.0.0.0:8000 --settings=core.settings.${enviroment}
