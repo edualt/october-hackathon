30e8c61c473364cf5fec84a5dd617ca4e448c6c9
edualt <eduardosebastianhdz503@gmail.com>
2022-10-20 20:17:36 -0500

    fix(entrypoint): fix error on line 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py wait_for_db --settings=core.settings.${enviroment}
 echo "Apply database migrations"
 python manage.py makemigrations --settings=core.settings.${enviroment}
 python manage.py migrate --settings=core.settings.${enviroment}
-python manage.py migrate --run-syncdb --settings=core.settings${enviroment}
+python manage.py migrate --run-syncdb --settings=core.settings.${enviroment}
 
 # Start server
 echo "Starting server"
