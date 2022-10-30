b359486c4715a70e073bf1cbe30bf297d48eeb72
edualt <eduardosebastianhdz503@gmail.com>
2022-10-29 21:44:41 -0500

    fix(entrypoint): change settings directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 python manage.py wait_for_db --settings=core.settings.${environment}
 
 echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
+python manage.py collectstatic --no-input --settings=core.settings.production
 
 # Apply database migrations
 echo "Apply database migrations"
