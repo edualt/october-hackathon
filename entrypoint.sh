cfdd003fe8834484df85a6b1451b655b89455990
edualt <eduardosebastianhdz503@gmail.com>
2022-10-29 20:25:27 -0500

    fix(entrypoint): change variable "enviroment" to "environment"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Check if database is up
-python manage.py wait_for_db --settings=core.settings.${enviroment}
+python manage.py wait_for_db --settings=core.settings.${environment}
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigrations --settings=core.settings.${enviroment}
-python manage.py migrate --settings=core.settings.${enviroment}
-python manage.py migrate --run-syncdb --settings=core.settings.${enviroment}
+python manage.py makemigrations --settings=core.settings.${environment}
+python manage.py migrate --settings=core.settings.${environment}
+python manage.py migrate --run-syncdb --settings=core.settings.${environment}
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:${PORT} --settings=core.settings.${enviroment}
+python manage.py runserver 0.0.0.0:${PORT} --settings=core.settings.${environment}
