ebf2f1bcd28e8e7751a113dc8092f45548ceb926
edualt <eduardosebastianhdz503@gmail.com>
2022-10-29 21:11:16 -0500

    fix(entrypoint)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Check if database is up
 python manage.py wait_for_db --settings=core.settings.${environment}
 
+echo 'Running collecstatic...'
+python manage.py collectstatic --no-input --settings=config.settings.production
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations --settings=core.settings.${environment}
